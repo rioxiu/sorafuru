0f72c1a5e01a02f77fed2411f155cb9de921d35e
rioxiu <richardosinulingga@outlook.jp>
2023-06-20 09:05:25 +0700

    create .env to keep secret item for init db

--- main.go
+++ main.go
@@ -3,24 +3,39 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"sorafuru/handlers"
 	"sorafuru/user"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	dsn := "root:otsukeris@tcp(127.0.0.1:3306)/sorafuru?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
+	//config database from .env file
+	err := godotenv.Load()
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Fatal("error loading .env")
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	dbConnectstring := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUsername, dbPassword, dbHost, dbPort, dbName)
+
+	dbConnection, err := gorm.Open(mysql.Open(dbConnectstring), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error connecting to the database:", err)
 	}
 
 	//user service dan handler
-	userRepository := user.NewRepository(db)
+	userRepository := user.NewRepository(dbConnection)
 	userService := user.NewService(userRepository)
 	userHandler := handlers.NewUserHandlers(userService)
 
